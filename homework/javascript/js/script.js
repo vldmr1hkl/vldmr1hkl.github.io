ccff441c80d2eb87084614ed3b51c28e5455bc22
vldmr1hkl <129081464+vldmr1hkl@users.noreply.github.com>
2023-04-28 02:21:42 +0300

    Update script.js

--- homework/javascript/js/script.js
+++ homework/javascript/js/script.js
@@ -1,6 +1,9 @@
 let content = document.getElementById('content')
 let userName = prompt("What is your name?");
 let userAge = prompt("How old are you?");
+while(isNaN(userAge)){
+	userAge = prompt("Enter your age in numerals only:");
+};
 userAge = Number(userAge);
 let userAgeIn5year = 5 + userAge;
 content.innerHTML = "Hello, " +userName+ "! In 5 years you will be " +userAgeIn5year+ " years old!";
