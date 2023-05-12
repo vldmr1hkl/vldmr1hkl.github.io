20869dd888585480a0c0401c0d183d502ed9704a
vldmr1hkl <129081464+vldmr1hkl@users.noreply.github.com>
2023-05-12 13:19:51 +0300

    Add files via upload

--- myproject/js/script.js
+++ myproject/js/script.js
@@ -1,12 +1,81 @@
-let iconMenu = document.getElementById('iconMenu')
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelectorAll(".jsCloseMenu");
+const expandBtn = document.querySelectorAll(".expand-btn");
 
-let iconOpen = document.getElementById('iconOpen')
-let iconClose = document.getElementById('iconClose')
+// humburger toggle
+menuBtn.addEventListener("click", () => {
+  //menuBtn.classList.toggle("open");
+  
+  document.body.classList.toggle('openedMenu')
+  //menuItems.classList.toggle("open");
+});
 
-let burger = document.getElementById('burger')
+// // mobile menu expand
+// expandBtn.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     btn.classList.toggle("open");
+//   });
+// });
 
-iconMenu.addEventListener('click', () => {
-	iconOpen.classList.toggle('hide')
-	iconClose.classList.toggle('hide')
-	burger.classList.toggle('show-menu')
-})
+
+menuItems.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.body.classList.remove('openedMenu')
+  });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 2,
+		// slidesToScroll:1,
+		speed: 1000,
+		// infinite: true,
+		// initialSlide: 0,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		// pauseOnFocus: true,
+		// pauseOnHover: true,
+		// pauseOnDotsHover: true,
+		draggable: false,
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		waitForAnimate: false,
+		// швидко перелистуються слайди
+		centerMode: true,
+		variableWidth: true,
+		// rows: 2,
+		// slidesPerRow: 2,
+		// vertical: false,
+		// verticalSwiping: true,
+		// fade: true,
+		// asNavFor: ".sliderbig",
+		// звязати два слайдера з одинаковою кількістю слайдів,
+		responsive:[
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+				dots: false,
+			}
+		}],
+		mobileFirst: false,
+		// appendArrows: $(".content"),
+		// appendDots: $(".content"),
+		// перемістити в іншій div
+
+	});
+	// $('.slider')on('beforeChange', function(event, slick, currentSlide, nextSide){
+
+	// })
+
+});
